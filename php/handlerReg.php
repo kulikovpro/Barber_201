be3673e3a81e0efdad887e7952bc9e5d351c860c
razorenova <razorenova.om@yandex.ru>
2021-04-26 09:58:34 +0300

    Reg

--- php/handlerReg.php
+++ php/handlerReg.php
@@ -1,15 +1,16 @@
 <?php
-  $mysqli = new mysqli('localhost','vladle43_0201','8O&Zjowm','vladle43_0201');
-  $name = $_POST['name'];
-  $lastname = $_POST['lastname'];
-  $email = trim(mb_strtolower($_POST['email']));
-  $pass = trim($_POST['pass']);
-  $pass = password_hash($pass, PASSWORD_DEFAULT);
-  $result = $mysqli->query("SELECT id FROM `users` WHERE `email`='$email'");
+  header("Access-Control-Allow-Origin: *");
+  $mysqli = new mysqli('localhost','v90377xk_0201','L6Ted&jv','v90377xk_0201');
+  $name = $_POST['register-form-name'];
+  $lastname = $_POST['register-form-username'];
+  $email = trim(mb_strtolower($_POST['register-form-email']));
+  $pass = trim($_POST['register-form-password']);
+  $pass = password_hash($register-form-password, PASSWORD_DEFAULT);
+  $result = $mysqli->query("SELECT id FROM `user` WHERE `register-form-email`='$email'");
   if($result->num_rows){
     echo json_encode(['result'=>'exist']);
   }else{
-    $mysqli->query("INSERT INTO `users`(`name`, `lastname`, `email`, `pass`) VALUES ('$name','$lastname','$email','$pass')");
+    $mysqli->query("INSERT INTO `user`(`register-form-name`, `register-form-username`, `register-form-email`, `register-form-password`) VALUES ('$name','$lastname','$email','$pass')");
     echo json_encode(['result'=>'success']);
   }
 ?>
